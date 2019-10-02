8ea1039bcb256f61b4f3bda480c7609c647e1c0c
Stack <TheOneAndOnlyStack@gmail.com>
2019-10-02 14:52:10 -0500

    Load It In!

--- Classwork/LoadImageTest/p5/LoadImage.js
+++ Classwork/LoadImageTest/p5/LoadImage.js
@@ -1,5 +1,11 @@
 var img;
 
+function preload(){
+
+    img=loadImage("https://github.com/TheOneAndOnlyStack/Vu_Stack_ART2210/raw/master/Classwork/LoadImageTest/p5/Image/IMG_3184.JPG");
+    
+}
+
 function setup(){
 
     createCanvas(windowWidth,windowHeight);
@@ -9,7 +15,9 @@ function setup(){
 
 function draw(){
 
-    img=loadImage("https://github.com/TheOneAndOnlyStack/Vu_Stack_ART2210/raw/master/Classwork/LoadImageTest/p5/Image/IMG_3184.JPG");
+    //background('#fae');
+
+    image(img,0,0);
 
     noStroke();
     fill(0);
@@ -18,7 +26,8 @@ function draw(){
 
 }
 
-function resize(){
+function windowResized(){
 
+    windowResized(windowWidth,windowHeight);
 
 }
