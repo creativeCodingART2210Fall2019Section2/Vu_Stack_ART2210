a9073f7c47b7ea4f4c831b7fb03ba5980e78bd6d
Stack <TheOneAndOnlyStack@gmail.com>
2019-10-02 14:43:47 -0500

    Czech

--- Classwork/LoadImageTest/p5/LoadImage.js
+++ Classwork/LoadImageTest/p5/LoadImage.js
@@ -11,6 +11,10 @@ function draw(){
 
     img=loadImage("https://github.com/TheOneAndOnlyStack/Vu_Stack_ART2210/raw/master/Classwork/LoadImageTest/p5/Image/IMG_3184.JPG");
 
+    noStroke();
+    fill(0);
+    rect(windowWidth/2,windowHeight/2,100,100);
+
 
 }
 
